960693edf9c94b3e6848ae72929559e4a6c5414c
Demetra Koras <demms@me.com>
2017-04-10 14:51:09 -0400

    Adds skills method to serializer

--- app/serializers/seeker_profile_serializer.rb
+++ app/serializers/seeker_profile_serializer.rb
@@ -1,6 +1,10 @@
 class SeekerProfileSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email, :phone, :matched_jobs
+  attributes :id, :first_name, :last_name, :email, :phone, :matched_jobs, :skills
+
+  def skills
+    object.skills
+  end
 
   belongs_to :user
-  has_many :skills
+  # has_many :skills
 end
