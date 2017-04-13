cc75c20c19b4156cb2fcd3dbe79b7173f3deceb5
Demetra Koras <demms@me.com>
2017-04-13 11:15:00 -0400

    Adds default to active column on jobs table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170411132713) do
+ActiveRecord::Schema.define(version: 20170413151332) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,11 +31,11 @@ ActiveRecord::Schema.define(version: 20170411132713) do
     t.string   "title"
     t.text     "description"
     t.boolean  "transportation"
-    t.boolean  "active"
+    t.boolean  "active",              default: true
     t.integer  "employer_profile_id"
     t.integer  "location_id"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
     t.index ["employer_profile_id"], name: "index_jobs_on_employer_profile_id", using: :btree
     t.index ["location_id"], name: "index_jobs_on_location_id", using: :btree
   end
