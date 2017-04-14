d53b0d43ab07d32c64b2f816f6baba0adc6e6478
Aubrey Badger <akbadger@gmail.com>
2017-04-14 12:05:22 -0400

    working on styling landing page

--- app-react/src/Home.js
+++ app-react/src/Home.js
@@ -12,7 +12,7 @@ class Home extends Component {
                 <h1>TURN</h1>
                 <h4 className="text-uppercase">connecting people with meaningful work</h4>
                 <br/><br/>
-                <h4 className="text-uppercase">Sign up</h4>
+                <h5 className="text-uppercase">Sign up</h5>
                 <button type="button" className="btn btn-transparent" onClick={() => browserHistory.push('/employersignup')}>I'm an employer</button>
                 <button type="button" className="btn btn-transparent" onClick={() => browserHistory.push('/seekersignup')}>I'm looking for a job</button>
             </div>
@@ -25,15 +25,15 @@ class Home extends Component {
                 <h4>Turn is an employement platform for connecting workers + employers</h4>*/}
             </div>
         </div>
-        <div className="row text-center">
+        <div className="row text-center about-section">
             <div className="col-sm-4 col-sm-offset-1">
-                    <h2>ABOUT US</h2>
-                    <h4>We don't think having a record should mean that you don't have access to financial stability through a job.  Our goal is to connect employers who share our conviction and people who need work.  Our hope is to help restore autonomy to folks getting back on their feet.</h4>
+                    <h3>ABOUT US</h3>
+                    <h5>We don't think having a record should mean that you don't have access to financial stability through a job.  Our goal is to connect employers who share our conviction and people who need work.  Our hope is to help restore autonomy to folks getting back on their feet. We're doing this with Turn, an employement platform for connecting workers and employers.</h5>
                     
             </div>
             <div className="col-sm-4 col-sm-offset-1">
-                    <h2>NEW LEAF</h2>
-                    <h4>New Leaf – New Life is a 501 c3 nonprofit organization that offers services to folks in the Monroe County Jail during incarceration and after release. Applying the philosophy of therapeutic justice, we encourage personal growth and self-advocacy, supporting incarcerated individuals in their efforts to make a successful transition back into the community.</h4>
+                    <h3>NEW LEAF</h3>
+                    <h5>New Leaf – New Life is a 501 c3 nonprofit organization that offers services to folks in the Monroe County Jail during incarceration and after release. Applying the philosophy of therapeutic justice, we encourage personal growth and self-advocacy, supporting incarcerated individuals in their efforts to make a successful transition back into the community.</h5>
             </div>
         </div>
     </div>
