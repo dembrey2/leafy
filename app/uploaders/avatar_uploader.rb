7322aacb425152aff0b3e43530bf6ffb3d17d940
Demetra Koras <demms@me.com>
2017-04-18 14:04:54 -0400

    Adds default image and crops

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -2,7 +2,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # include Cloudinary::CarrierWave
 
@@ -26,12 +26,17 @@ class AvatarUploader < CarrierWave::Uploader::Base
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
 
+  def default_url(*args)
+    Rails.root.join("public/img/lovelace.jpg").open
+  end
+
   # Process files as they are uploaded:
   # process scale: [200, 300]
   #
   # def scale(width, height)
   #   # do something
   # end
+  process resize_to_fit: [800, 800]
 
   # Create different versions of your uploaded files:
   # version :thumb do
