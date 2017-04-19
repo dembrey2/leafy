3e7b720457fd0885536f960588f47bc374829aa5
Demetra Koras <demms@me.com>
2017-04-19 11:00:28 -0400

    Updates seeker serializer to reflect text/email preference

--- app/serializers/seeker_profile_serializer.rb
+++ app/serializers/seeker_profile_serializer.rb
@@ -1,5 +1,5 @@
 class SeekerProfileSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email, :phone, :skills, :work_history, :education, :interests, :preferred_contact
+  attributes :id, :first_name, :last_name, :email, :phone, :skills, :work_history, :education, :interests, :preferred_contact, :text_me, :email_me
 
   def skills
     object.skills
