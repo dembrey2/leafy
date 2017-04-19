6cd8afa8d0dfe0f32a6ff7443a5c09cd3e978937
Demetra Koras <demms@me.com>
2017-04-19 11:27:27 -0400

    Adds avatar to job serializer

--- app/serializers/job_serializer.rb
+++ app/serializers/job_serializer.rb
@@ -1,5 +1,5 @@
 class JobSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :transportation, :active, :user_id, :company_name, :location, :skills
+  attributes :id, :title, :description, :transportation, :active, :user_id, :company_name, :location, :skills, :avatar
 
   def company_name
     object.employer_profile.company_name
@@ -15,5 +15,9 @@ class JobSerializer < ActiveModel::Serializer
 
   def user_id
     object.employer_profile.user_id
+  end
+
+  def avatar
+    object.employer_profile.user.avatar
   end
 end
