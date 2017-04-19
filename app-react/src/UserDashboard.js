77d78ee28646dc5dd4ed8fc82c71d753676a0ad5
Aubrey Badger <akbadger@gmail.com>
2017-04-19 16:28:43 -0400

    adding to empty seeker dashboard

--- app-react/src/UserDashboard.js
+++ app-react/src/UserDashboard.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import UserSnapshot from './UserSnapshot'
 import JobSnapshot from './JobSnapshot'
+import { browserHistory } from 'react-router';
+
 
 class UserDashboard extends Component {
   componentWillMount() {
@@ -17,7 +19,14 @@ class UserDashboard extends Component {
         <div className="row">
           <div className="col-sm-6 col-sm-offset-3">
             <h3 className="text-center text-uppercase">Jobs</h3>
-            {matchedJobs}
+            {matchedJobs.length === 0 ? 
+              <div className="panel panel-default">
+                <div className="panel-body">
+                  <button type="button" className="btn btn-default btn-transparent-charcoal text-center btn-lg" onClick={() => browserHistory.push('/profile-edit')}>Click to edit your profile to view your job matches</button> 
+                </div>
+              </div>
+            
+            : {matchedJobs}}
           </div>
         </div>
       </div>
